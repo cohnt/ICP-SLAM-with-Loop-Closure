fe111d03983b0d1ff9327c57cf487bc292566d6c
Thomas Cohn <cohnt@umich.edu>
2022-03-29 11:48:47 -0400

    First pass at the code for the pose graph, along with some more documentation.

--- src/pose_graph.py
+++ src/pose_graph.py
@@ -9,13 +9,21 @@ import networkx as nx
 #
 # T is the transformation from (1) to (2)
 
-class pose_graph():
+# To iterate over the constraints in a pose graph, use something like
+# for edge in pose_graph.graph.edges.data('object'):
+#      do stuff...
+# Each "edge" will be a 3-tuple (i, j, transformation) where transfomation is the 3x3 matrix transformation
+# from node i to node j.
+
+class PoseGraph():
 	def __init__(self, transformations):
 		# transformations should be an (n-1, 3, 3) numpy array, where the ith entry is the SE(3) matrix
 		# of the transformation from pose i to pose i+1. Returns a pose graph.
-		pass
+		self.graph = nx.DiGraph()
+		for i in range(0, transformations.size[0]):
+			self.graph.add_edge(i, i+1, object=transformations[i])
 
 	def add_constraint(self, i, j, transformation):
 		# Adds the transformation from i to j into the graph object
 		# TODO: Make sure this is by reference!
-		pass
+		self.graph.add_edge(i, j, object=transformation)
