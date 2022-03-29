c9e2cec4e5dcb4f59aa7de2fc994cc178abe9cbc
Thomas Cohn <cohnt@umich.edu>
2022-03-29 14:56:28 -0400

    First pass at various drawing functions.

--- src/visualization.py
+++ src/visualization.py
@@ -1,18 +1,31 @@
 import matplotlib.pyplot
 
-def draw_occupancy_grid(ax, occupancy_grid):
+def draw_occupancy_grid(ax, occupancy_grid, cell_size, origin_location):
 	# Draw an occupancy grid
-	pass
+	# occupancy_grid should be a (h, w) numpy array of integers between -256 and 255.
+	# cell_size is the width of the occupancy grid cells (in meters)
+	# origin_location is the (x, y) coordinates of the center of the bottom-left cell in occupancy_grid
+	# Useful reference: https://matplotlib.org/3.5.0/api/_as_gen/matplotlib.pyplot.imshow.html
+	# Will need to use the "extent" argument
+	height = occupancy_grid.shape[0] * cell_size
+	width = occupancy_grid.shape[1] * cell_size
+	half_cell_size = cell_size / 2.0
+	left = origin_location[0] - half_cell_size
+	right = origin_location[0] + width - half_cell_size
+	bottom = origin_location[1] - half_cell_size
+	top = origin_location[1] + height - half_cell_size
+	ax.imshow(occupancy_grid, origin="lower", extent=(left, right, bottom, top), cmap=plt.get_cmap("binary"))
 
 def draw_path(ax, path):
 	# Draw the robot's path
-	pass
+	# path should be an (n, 2) numpy array, encoding the (x, y) robot positions
+	ax.plot(path[:,0], path[:,1], color="blue")
 
 def draw_pose_graph(ax, pose_graph, node_positions):
 	# Draw the full pose graph (with all additional constraints)
 	# node_positions should be an (n, 2) numpy array, encoding 
 	# the (x, y) position of each of the nodes.
-	
+
 	# First, draw the edges
 	for edge in pose_graph.graph.edges:
 		ax.plot(node_positions[edge,0], node_positions[edge, 1], color="red")
@@ -23,5 +36,16 @@ def draw_pose_graph(ax, pose_graph, node_positions):
 def draw_icp_iteration(ax, pc1, pc2, correspondences=None):
 	# Draws the output of ICP at a single iteration, centered on
 	# pc1, showing both point clouds, and optionally the correspondences
-	# between them.
-	pass
+	# between them. If it's given, correspondences should be an (n, 2)
+	# numpy array of integers, showing the matched pairs.
+
+	# First, draw the matches (if specified)
+	if correspondences != None:
+		for i, j in correspondences:
+			xs = [pc1[i,0], pc2[i,0]]
+			ys = [pc1[i,1], pc2[i,1]]
+			ax.plot(xs, ys, color="black")
+
+	# Then, draw the two point clouds
+	ax.scatter(pc1[:,0], pc1[:,1], color="red")
+	ax.scatter(pc2[:,0], pc2[:,1], color="blue")
