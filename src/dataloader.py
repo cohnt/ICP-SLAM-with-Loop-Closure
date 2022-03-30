eece6266c3beb7088c97a7a805152198ad20aa3c
John Rosner <Johntherobot5000@gmail.com>
2022-03-30 18:44:56 -0400

    Started work on Dataloader, not done or tested

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,4 +1,46 @@
+import cv2
 import numpy as np
+import lcm
+from lcmtypes import odometry_t, lidar_t
+
+
+def read_img(path):
+	image = cv2.imread(path, cv2.COLOR_BGR2RGB)
+	return image
+
+
+def get_images(data_folder_name):
+	timestamps_file = open(f'{data_folder_name}/image_timestamps.txt', 'r')
+	lines = timestamps_file.readlies()
+	imgs = np.empty((0, 480, 640, 3), dtype=float)
+	timestamps = np.empty((0), dtype=float)
+	
+	for line in lines:
+		n, time = line.split(", ")
+		img = read_img(f'{data_folder_name}/raw_images/image{n}.png')
+		imgs = np.append(imgs, img)
+		timestamps = np.append(timestamps, time)
+		
+	timestamps -= timestamps[0]
+
+	return imgs, timestamps
+
+
+def get_all_lcm_data(data_folder_name):
+	odometry = np.empty((0, 3), dtype=float)
+	point_cloud = np.empty((0, 0, 2), dtype=float)
+	init = False
+	
+	log = lcm.EventLog(f'{data_folder_name}/lcm.log', "r")
+	
+	for event in log:
+		if event.channel == "ODOMETRY":
+			msg = odometry_t.decode(event.data)
+			if not init:
+				start_time = msg.utime
+				init = True
+			odometry = np.append(odometry, np.array([msg.utime - start_time, msg.x, msg.y, msg.theta], axis = 0)	
+		
 
 def parse_lcm_log(data_folder_name, start_time=0, stop_time=np.inf, load_images=True):
 	# This script should read in the log file and images, matches lidar and odometry data
