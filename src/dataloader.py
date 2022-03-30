a5cdb8eae2d6fab3df5a1cda58fc3c5be7e5e9b1
Thomas Cohn <cohnt@umich.edu>
2022-03-30 14:28:02 -0400

    Update the documentation for the dataloader.

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,7 +4,7 @@ def parse_lcm_log(data_folder_name, start_time=0, stop_time=np.inf, load_images=
 	# This script should read in the log file and images, matches lidar and odometry data
 	# to each image, and then returns three things:
 	# (1) An (n, 3) numpy array of odometry values (x, y, theta)
-	# (2) An (n, m, 2) numpy array of point cloud lidar data -- each m-by-2 entry of the length-n
+	# (2) A length-n array of (m_i, 2) point cloud lidar data -- each m_i-by-2 entry of the length-n
 	#     list is in its own local coordinate frame (i.e. the robot center is at 0,0, and the
 	#     forward direction is positive x)
 	# (3) An (n, h, w, 3) numpy array of camera images
