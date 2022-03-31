4f47bafeead68bd3545e76d1bde857a5c27f64d0
Thomas Cohn <cohnt@umich.edu>
2022-03-31 13:28:57 -0400

    Modify produce_occupancy_grid to have a minimum size setting, and to return the location of the grid origin.

--- src/produce_occupancy_grid.py
+++ src/produce_occupancy_grid.py
@@ -7,16 +7,18 @@ import src.utils as utils
 # a map (for use with the robot), we flip it so that each row moves "down" in the
 # world.
 
-def produce_occupancy_grid(poses, lidar_points, cell_width, kHitOdds=3, kMissOdds=1):
+def produce_occupancy_grid(poses, lidar_points, cell_width, min_width=0, min_height=0, kHitOdds=3, kMissOdds=1):
 	# Produce an occupancy grid from range data
 	# poses should be an (n, 3) numpy array of poses (x, y, theta)
 	# lidar_points should be an length-n array of (m_i, 2) numpy arrays of point cloud
 	#    lidar data -- each m_i-by-2 entry of the length-n list is in
 	#    its own local coordinate frame (i.e. the robot center is at
 	#    0,0, and the forward direction is positive x)
+	# min_width and min_height are the minimum map sizes, in meters
 	# containing the complete map (with each cell being cell_width)
 	# The occupancy grid will be a (h, w) numpy array of integers between
 	# -128 and 127, with dtype np.int8
+	# Also returns the minimum x and y values of the grid
 
 	n = poses.shape[0]
 	ms = [len(lidar_points[i]) for i in range(len(lidar_points))]
@@ -36,6 +38,17 @@ def produce_occupancy_grid(poses, lidar_points, cell_width, kHitOdds=3, kMissOdd
 	max_y = np.max(all_points) + (cell_width / 2)
 	width_dist = max_x - min_x
 	height_dist = max_y - min_y
+
+	if width_dist < min_width:
+		offset = (min_width - width_dist) / 2
+		min_x -= offset
+		max_x += offset
+		width_dist = min_width
+	if height_dist < min_height:
+		offset = (min_height - height_dist) / 2
+		min_y -= offset
+		max_y += offset
+		height_dist = min_height
 
 	width_in_cells = np.ceil(width_dist / cell_width).astype(int)
 	height_in_cells = np.ceil(height_dist / cell_width).astype(int)
@@ -79,7 +92,7 @@ def produce_occupancy_grid(poses, lidar_points, cell_width, kHitOdds=3, kMissOdd
 				else:
 					occupancy_grid[y0, x0] = 127
 
-	return occupancy_grid
+	return occupancy_grid, (min_x, min_y)
 
 def global_position_to_grid_cell(pos, min_x, min_y, cell_width):
 	# Given an (x, y) position pos, as well as various information about
