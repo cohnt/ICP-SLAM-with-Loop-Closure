99774496b7c4b3d8b4fd37a84c5d59f638af02fa
Thomas Cohn <cohnt@umich.edu>
2022-03-31 13:45:49 -0400

    Also draw the robot path.

--- scripts/test_map_odometry_only.py
+++ scripts/test_map_odometry_only.py
@@ -20,13 +20,14 @@ cell_width = 0.05
 og, (min_x, min_y) = produce_occupancy_grid.produce_occupancy_grid(odometry[:start], lidar_points[:start], cell_width, min_width=10, min_height=10)
 
 fig, ax = plt.subplots()
-visualization.draw_occupancy_grid(ax, og, cell_size=0.05, origin_location=np.array([0, 0]))
+visualization.draw_occupancy_grid(ax, og, cell_size=0.05, origin_location=np.array([min_x, min_y]))
 plt.draw()
 plt.pause(0.001)
 
-skip = 5
+skip = 10
 for i in range(start, len(odometry), skip):
 	og = produce_occupancy_grid.update_occupancy_grid(og, odometry[i:i+skip], lidar_points[i:i+skip], cell_width, min_x, min_y)
-	visualization.draw_occupancy_grid(ax, og, cell_size=0.05, origin_location=np.array([0, 0]))
+	visualization.draw_occupancy_grid(ax, og, cell_size=0.05, origin_location=np.array([min_x, min_y]))
+	visualization.draw_path(ax, odometry[:i+skip])
 	plt.draw()
 	plt.pause(0.001)
