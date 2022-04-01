65fc4664e14874243d6de777a57cfd18bda3f0db
Thomas Cohn <cohnt@umich.edu>
2022-04-01 14:08:09 -0400

    Messy and doesn't work, John please fix and add documentation.

---
+++ scripts/test_icp.py
@@ -0,0 +1,56 @@
+#######################
+# Fix import issues   #
+import sys            #
+sys.path.append(".")  #
+sys.path.append("..") #
+#######################
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import special_ortho_group
+
+import src.visualization as visualization
+import src.icp as icp
+
+pc1 = (np.random.random(size=(20,2)) - 0.5) * 20
+pc1 = np.hstack((pc1, np.ones((len(pc1),1)))).T
+
+translation = np.random.random(size=(1,2))
+# rotation = special_ortho_group.rvs(2)
+theta = (np.random.random() - 0.5) * 0.1
+c = np.cos(theta)
+s = np.sin(theta)
+rotation = np.array([
+	[c, -s],
+	[s, c]
+])
+
+transformation_mat = np.eye(3)
+transformation_mat[:2,:2] = rotation
+transformation_mat[:2,2] = translation
+
+pc2 = transformation_mat @ pc1
+
+# fig, ax = plt.subplots()
+# ax.set_aspect("equal")
+# ax.scatter(pc1[0,:], pc1[1,:], color="red")
+# ax.scatter(pc2[0,:], pc2[1,:], color="blue")
+# plt.show()
+
+list_of_transforms = icp.icp(pc1.T, pc2.T)
+print(list_of_transforms)
+
+fig, ax = plt.subplots()
+ax.set_aspect("equal")
+ax.scatter(pc1[0,:], pc1[1,:], color="red")
+ax.scatter(pc2[0,:], pc2[1,:], color="blue")
+plt.draw()
+plt.pause(0.001)
+for tf in list_of_transforms:
+	print(tf)
+	temp = tf @ pc1
+	# corresp = icp.correspondences(temp, pc2)
+	visualization.draw_icp_iteration(ax, temp.T, pc2.T)
+	plt.draw()
+	plt.pause(0.5)
+plt.show()
--- src/icp.py
+++ src/icp.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def get_closest_point(point, pc):
-	distances = np.sum((pc - point) ** 2, axis=0)
+	distances = np.sum((pc - point) ** 2, axis=1)
 	return np.argmin(distances)
 
 
 def get_correspondences(pc1, pc2):
-	correspondences = np.zeros(pc1.shape[0])
+	correspondences = np.zeros(pc1.shape[0], dtype=int)
 	for i in range(pc2.shape[0]):
 		correspondences[i] = get_closest_point(pc1[i], pc2)
 
@@ -15,25 +15,26 @@ def get_correspondences(pc1, pc2):
 
 def get_transform(pc1, pc2, correspondences):
 	pc2 = pc2[correspondences]
-	pc1_avg = np.sum(pc1, axis=1) / pc1.shape[0]
-	pc2_avg = np.sum(pc2, axis=1) / pc2.shape[0]
+	pc1_avg = np.sum(pc1, axis=0) / pc1.shape[0]
+	pc2_avg = np.sum(pc2, axis=0) / pc2.shape[0]
 
-	X = pc1[:, 0:2] - pc1_avg
-	Y = pc2[:, 0:2] - pc2_avg
+	X = pc1[:, 0:2] - pc1_avg[:2]
+	Y = pc2[:, 0:2] - pc2_avg[:2]
 
-	S = X @ Y.T
-	U, sigma, V_t = np.linalg.svd(S)
+	S = X.T @ Y
+	U, sigma, V_t = np.linalg.svd(S, full_matrices=False)
 	V = V_t.T
+
 
 	mid_thingy = np.eye(2)
 	mid_thingy[1, 1] = np.linalg.det(V @ U.T)
 	R = V @ mid_thingy @ U.T
-	t = pc2_avg - R @ pc1_avg
+	t = pc2_avg - R @ pc1_avg[:2].reshape(-1,1)
 
-	transformed = np.eye(3)	
+	transformed = np.eye(3)
 	transformed[0:2, 0:2] = R
-	transformed[2, 0] = t[0]
-	transformed[2, 1] = t[1]
+	transformed[2, 0] = t[0,0]
+	transformed[2, 1] = t[1,0]
 
 	return transformed
 
@@ -47,19 +48,19 @@ def icp(pc1, pc2, init_transform=np.eye(3), epsilon=0.01):
 	# init_transform is a 3x3 matrix in SE(2), that provides the initialization
 	# for ICP. Returns a 3x3 matrix in SE(2) that estimates the final transformation
 
-	# Convert to Homogeneous coordinates
-	pc1 = np.hstack((pc1, np.zeros(pc1.shape[0], 1)))
-	pc2 = np.hstack((pc2, np.zeros(pc2.shape[0], 1)))
-
-	transforms = init_transform
+	transforms = [init_transform]
 	iteration = 0
+	max_iters = 100
 
 	while True:
 		transformed = np.dot(transforms[-1], pc1.T).T
 		correspondences = get_correspondences(transformed, pc2)
 		transformation = get_transform(transformed, pc2, correspondences)
-		transforms = np.append(transforms, transformation)
+		transforms.append(transformation)
+		print(get_error(transformed, pc2, correspondences))
 		if get_error(transformed, pc2, correspondences) < epsilon:
+			return transforms
+		if iteration > max_iters:
 			return transforms
 		iteration += 1
 
