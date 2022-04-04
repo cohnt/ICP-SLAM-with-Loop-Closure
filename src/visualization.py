9529c15ca537d9a997c495c2eae4b9ba77f68e57
Thomas Cohn <cohnt@umich.edu>
2022-04-03 20:33:04 -0400

    Low-key it looks better without drawing the nodes.

--- src/visualization.py
+++ src/visualization.py
@@ -21,7 +21,7 @@ def draw_path(ax, path):
 	# path should be an (n, 2) numpy array, encoding the (x, y) robot positions
 	ax.plot(path[:,0], path[:,1], color="blue")
 
-def draw_pose_graph(ax, pose_graph, node_positions):
+def draw_pose_graph(ax, pose_graph, node_positions, draw_nodes=False):
 	# Draw the full pose graph (with all additional constraints)
 	# node_positions should be an (n, 2) numpy array, encoding 
 	# the (x, y) position of each of the nodes.
@@ -31,7 +31,8 @@ def draw_pose_graph(ax, pose_graph, node_positions):
 		ax.plot(node_positions[edge,0], node_positions[edge, 1], color="red")
 
 	# Next, draw the nodes
-	ax.scatter(node_positions[:,0], node_positions[:,1], color="red")
+	if draw_nodes:
+		ax.scatter(node_positions[:,0], node_positions[:,1], color="red")
 
 def draw_icp_iteration(ax, pc1, pc2, correspondences=[]):
 	# Draws the output of ICP at a single iteration, centered on
