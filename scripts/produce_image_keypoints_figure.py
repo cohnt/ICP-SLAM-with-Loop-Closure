f93fb458964c1051f76fe4b3f0013adff90a0660
Thomas Cohn <cohnt@umich.edu>
2022-04-16 17:08:18 -0400

    Script to grab they keypoints from an image, and save the image annotated with the keypoints. Used for the presentation/report.

---
+++ scripts/produce_image_keypoints_figure.py
@@ -0,0 +1,21 @@
+#######################
+# Fix import issues   #
+import sys            #
+import traceback      #
+sys.path.append(".")  #
+sys.path.append("..") #
+#######################
+
+import numpy as np
+import cv2
+
+from src.loop_closure_detection import find_keypoints, deserialize_keypoints
+
+img_fname = "./data/EECS_3/raw_images/image0.png"
+img = cv2.imread(img_fname, 0)
+
+kp, des = deserialize_keypoints(find_keypoints(img))
+img2 = cv2.drawKeypoints(img, kp, None, color=(0,255,0), flags=0)
+
+cv2.imwrite("raw.png", img)
+cv2.imwrite("keypoints.png", img2)
