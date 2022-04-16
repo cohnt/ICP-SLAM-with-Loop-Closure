51ca92aad7f43e28228c6198ab45e431e8488eab
Thomas Cohn <cohnt@umich.edu>
2022-04-16 18:30:00 -0400

    Some small issues with this script.

--- scripts/produce_loop_closure_icp_figure.py
+++ scripts/produce_loop_closure_icp_figure.py
@@ -15,7 +15,7 @@ import src.visualization as visualization
 print("Loading the data")
 odometry, lidar_points, images = dataloader.parse_lcm_log("./data/EECS_3", load_images=True, image_stop=np.inf, n_jobs=-1)
 
-pc1 = lidar_points[50]
+pc1 = lidar_points[56]
 pc1 = np.hstack((pc1, np.ones((len(pc1),1)))).T
 pc2 = lidar_points[182]
 pc2 = np.hstack((pc2, np.ones((len(pc2),1)))).T
@@ -34,7 +34,7 @@ for i in range(30):
 	temp = next_transform @ pc1
 	transforms.append(next_transform)
 	visualization.draw_icp_iteration(ax, temp.T, pc2.T, correspondences)
-	plt.savefig("iter%d.png" % (i+1))
+	plt.savefig("iter%02d.png" % (i+1))
 	print(error)
 	if error < 0.01:
 		break
